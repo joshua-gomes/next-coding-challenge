5d62554436df9293c9ea3e0e10b0d0759f8409c0
Joshimoto <joshuagomes97@gmail.com>
2024-05-22 00:29:11 +0100

    Called Basket component on the Next homepage and removed references of basket styling on the page's css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,47 +1,89 @@
-'use client';
-import { useState } from 'react';
-import { CartItem } from '@/types/cart';
-import styles from './page.module.css'
+"use client";
+import { useState } from "react";
+import { CartItem } from "@/types/cart";
+import Basket from "@/components/Basket";
+import styles from "./page.module.css";
 
-function ItemCount({count, name}: {count: number, name: string}) {
-  return <div key={name}>{name} count: {count}</div>
-};
+const itemsList = [
+  {
+    name: "Item 1",
+    blurb: "Foo",
+  },
+  {
+    name: "Item 2",
+    blurb: "Bar",
+  },
+  {
+    name: "Item 3",
+    blurb: "Baz",
+  },
+  {
+    name: "Item 4",
+    blurb: "Qux",
+  },
+];
 
 export default function Home() {
   const [items, setItems] = useState<CartItem[]>([]);
-  const [itemCount, setItemCount] = useState<number>(0);
 
-  const addToCart = (product: string) => {
-    const alreadyInCart = items.find(item => item.name === product);
+  const handleOnClickAddToCart = (product: string) => {
+    const alreadyInCart = items.find((item) => item.name === product);
     if (alreadyInCart) {
       // @TODO need to find out how to update cart items
     } else {
       setItems([...items, { name: product, quantity: 1 }]);
     }
-    setItemCount(itemCount + 1);
-  }
+  };
 
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>
-          Michael&apos;s Amazing Web Store
-        </p>
-        <div>
-        <button className={styles.basket}>Basket: {itemCount} items</button>
-          <ItemCount name="Item 1" count={items.find(item=> item.name === 'Item 1')?.quantity || 0}/>
-          <ItemCount name="Item 2" count={items.find(item=> item.name === 'Item 2')?.quantity || 0}/>
-          <ItemCount name="Item 3" count={items.find(item=> item.name === 'Item 3')?.quantity || 0}/>
-          <ItemCount name="Item 4" count={items.find(item=> item.name === 'Item 4')?.quantity || 0}/>
-        </div>
+        <p>Michael&apos;s Amazing Web Store</p>
+        <Basket items={items} />
       </div>
 
       <div className={styles.grid}>
-        <button className={styles.card} onClick={() => addToCart('Item 1')} aria-label="Add to basket"><h2>Item 1 <span>-&gt;</span></h2><p>Foo</p></button>
-        <button className={styles.card} onClick={() => addToCart('Item 2')} aria-label="Add to basket"><h2>Item 2 <span>-&gt;</span></h2><p>Bar</p></button>
-        <button className={styles.card} onClick={() => addToCart('Item 3')} aria-label="Add to basket"><h2>Item 3 <span>-&gt;</span></h2><p>Baz</p></button>
-        <button className={styles.card} onClick={() => addToCart('Item 4')} aria-label="Add to basket"><h2>Item 4 <span>-&gt;</span></h2><p>Qux</p></button>
+        <button
+          className={styles.card}
+          onClick={() => handleOnClickAddToCart("Item 1")}
+          aria-label="Add to basket"
+        >
+          <h2>
+            Item 1 <span>-&gt;</span>
+          </h2>
+          <p>Foo</p>
+        </button>
+        <button
+          className={styles.card}
+          onClick={() => handleOnClickAddToCart("Item 2")}
+          aria-label="Add to basket"
+        >
+          <h2>
+            Item 2 <span>-&gt;</span>
+          </h2>
+          <p>Bar</p>
+        </button>
+        <button
+          className={styles.card}
+          onClick={() => handleOnClickAddToCart("Item 3")}
+          aria-label="Add to basket"
+        >
+          <h2>
+            Item 3 <span>-&gt;</span>
+          </h2>
+          <p>Baz</p>
+        </button>
+        <button
+          className={styles.card}
+          onClick={() => handleOnClickAddToCart("Item 4")}
+          aria-label="Add to basket"
+        >
+          <h2>
+            Item 4 <span>-&gt;</span>
+          </h2>
+          <p>Qux</p>
+        </button>
       </div>
     </main>
-  )
+  );
 }
