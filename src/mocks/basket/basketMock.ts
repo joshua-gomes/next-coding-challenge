ad00cf13ce13ffa2b1c131802e5374232393cca8
Joshimoto <joshuagomes97@gmail.com>
2024-05-22 21:39:10 +0100

    Added basket mock

---
+++ src/mocks/basket/basketMock.ts
@@ -0,0 +1,14 @@
+export const basketMock = [
+  {
+    name: "Item 1",
+    quantity: 2,
+  },
+  {
+    name: "Item 2",
+    quantity: 5,
+  },
+  {
+    name: "Item 3",
+    quantity: 3,
+  },
+];
